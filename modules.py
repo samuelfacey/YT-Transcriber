0cfda7e865a15320c336c6ee098bbddd75861685
SamuelFacey <epicsam75@outlook.com>
2022-12-19 18:55:22 -0500

    Fixed text not being translated if too long

--- modules.py
+++ modules.py
@@ -5,6 +5,7 @@ import os
 import shutil
 from googletrans import Translator
 from unidecode import unidecode
+import traceback
 
 class Methods:
     def __init__(self) -> None:
@@ -55,7 +56,7 @@ class Methods:
         return f'{sub_info[0]}.txt'
 
 
-    def translate(self, text_file:str, lang:str, encoding:str):
+    def translate(self, text_file:str, language:str, encoding:str):
 
         t = Translator()
         with open(text_file, 'r') as file:
@@ -65,11 +66,12 @@ class Methods:
             print('Sorry, this file is too large to be translated.')
             return '[No file created]'
         
-        translation_output = t.translate(text=transcript, dest=lang)
-        translation_text:str = translation_output.text
-    
-        translated_file = f'[{lang.upper()}] {text_file}'
         
+        translation_text = self.call_translate(text=transcript, language=language)
+    
+        translated_file = f'[{language.upper()}] {text_file}'
+        
+
         with open(translated_file, 'w') as file:
             if encoding == '2':
                 file.write(unidecode(translation_text))
@@ -80,6 +82,33 @@ class Methods:
         return translated_file
 
 
+    def call_translate(self, text, language):
+        t = Translator()
+        list_of_segments = []
+        text_container = []
+        new_list_of_segments = []
+        num_of_lines = 0
+
+        for line in text:
+            text_container.append(line)
+            if '\n' in line:
+                num_of_lines += 1
+            if num_of_lines == 100:
+                list_of_segments.append(text_container)
+                num_of_lines = 0
+                text_container = []
+        
+        for i in list_of_segments:
+            text_segment = ''.join(i)
+            translation_output = t.translate(text=text_segment, dest=language)
+            translation_text:str = translation_output.text
+            new_list_of_segments.append(translation_text)
+        
+        translated_transcript = ''.join(new_list_of_segments)
+
+        return translated_transcript
+
+
     def run(self):
         link_prompt = input('Enter your YouTube link here: ')
         starting_prompt = input('Would you like to translate the text into another language? 1: Yes, 2: No\n')
@@ -88,7 +117,7 @@ class Methods:
             if starting_prompt == '1':
                 lang_prompt = input('What Language would you like to translate to? ')
                 encoding_prompt = input('Would you like to keep the accented characters? May not work properly on english devices. 1: Yes, 2: No\n')
-                print(f'Your file: {self.translate(text_file=self.transcribe(link=link_prompt), lang=lang_prompt, encoding=encoding_prompt)}, has been created!')
+                print(f'Your file: {self.translate(text_file=self.transcribe(link=link_prompt), language=lang_prompt, encoding=encoding_prompt)}, has been created!')
                 input('Press enter to quit.')
             elif starting_prompt == '2':
                 print(f'Your file: {self.transcribe()}, has been created!')
@@ -96,6 +125,6 @@ class Methods:
             else:
                 print('Invalid input.')
                 self.run()
-        except Exception as e:
-            print(e)
+        except Exception:
+            traceback.print_exc()
             self.run()
